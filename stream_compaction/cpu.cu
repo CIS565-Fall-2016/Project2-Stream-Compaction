6fba7eb50e9b903a2e3dab2cef465e44785eafe2
Gabe Naghi <gabenaghi@users.noreply.github.com>
2016-09-15 20:55:48 -0400

    CPU implementations

--- stream_compaction/cpu.cu
+++ stream_compaction/cpu.cu
@@ -8,8 +8,14 @@ namespace CPU {
  * CPU scan (prefix sum).
  */
 void scan(int n, int *odata, const int *idata) {
-    // TODO
-    printf("TODO\n");
+	
+	//exclusive scan: first element of output is 0
+	odata[0] = 0;
+
+	for (int i = 0; i < n - 1; ++i)
+	{
+		odata[i + 1] = odata[i] + idata[i];
+	}
 }
 
 /**
@@ -18,8 +24,19 @@ void scan(int n, int *odata, const int *idata) {
  * @returns the number of elements remaining after compaction.
  */
 int compactWithoutScan(int n, int *odata, const int *idata) {
-    // TODO
-    return -1;
+    
+	int oIndex = 0;
+
+	for (int i = 0; i < n; ++i)
+	{
+		if (idata[i])
+		{
+			odata[oIndex] = idata[i];
+			++oIndex;
+		}
+	}
+
+	return oIndex;
 }
 
 /**
@@ -28,8 +45,37 @@ int compactWithoutScan(int n, int *odata, const int *idata) {
  * @returns the number of elements remaining after compaction.
  */
 int compactWithScan(int n, int *odata, const int *idata) {
-    // TODO
-    return -1;
+	
+	int *temp = new int[n];
+	int *scanResult = new int[n];
+	// compute the temp array
+	for (int i = 0; i < n; ++i)
+	{
+		if (idata[i])
+			temp[i] = 1;
+		else
+			temp[i] = 0;
+	}
+
+	// Run exclusive scan on temp array
+	scan(n, scanResult, temp);
+
+	// result of scan is index into final array
+	int oCnt = 0;
+	for (int i = 0; i < n; ++i)
+	{
+		// only write if tmp array has 1
+		if (temp[i])
+		{
+			odata[scanResult[i]] = idata[i];
+			++oCnt;
+		}
+	}
+
+	delete[] temp;
+	delete[] scanResult;
+
+	return oCnt;
 }
 
 }
