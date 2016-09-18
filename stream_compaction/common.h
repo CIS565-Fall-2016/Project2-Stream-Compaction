83bdacc9d6bf32857982566b71fb21211677ce06
Gabe Naghi <gabenaghi@users.noreply.github.com>
2016-09-18 15:12:51 -0400

    initial naive GPU implementation. Untested due to *invalid device function*

--- stream_compaction/common.h
+++ stream_compaction/common.h
@@ -7,6 +7,8 @@
 #define FILENAME (strrchr(__FILE__, '/') ? strrchr(__FILE__, '/') + 1 : __FILE__)
 #define checkCUDAError(msg) checkCUDAErrorFn(msg, FILENAME, __LINE__)
 
+#define blocksize 128
+
 /**
  * Check for CUDA errors; print and exit if there was a problem.
  */
--- stream_compaction/naive.cu
+++ stream_compaction/naive.cu
@@ -6,14 +6,51 @@
 namespace StreamCompaction {
 namespace Naive {
 
-// TODO: __global__
+	__global__ void parallelAdd(int n, int depth, int* odata, int* idata)
+{
+	int k = (blockIdx.x * blockDim.x) + threadIdx.x;
+
+	if (k >= n || k < (int) powf(2, depth - 1))
+		return;
+
+	odata[k] = idata[k - (int)powf(2, depth - 1)] + idata[k];
+}
 
 /**
  * Performs prefix-sum (aka scan) on idata, storing the result into odata.
  */
 void scan(int n, int *odata, const int *idata) {
-    // TODO
-    printf("TODO\n");
+    
+	dim3 numBlocks = (n + blocksize - 1) / blocksize;
+
+	int * dev_beforeScan;
+	int * dev_afterScan;
+
+	cudaMalloc((void **)&dev_beforeScan, n * sizeof(int));
+	checkCUDAError("cudaMalloc dev_beforeScan failed!");
+	cudaMalloc((void **)&dev_afterScan, n * sizeof(int));
+	checkCUDAError("cudaMalloc dev_afterScan failed!");
+
+	cudaMemcpy(dev_beforeScan, idata, sizeof(int) * n, cudaMemcpyHostToDevice);
+	checkCUDAError("cudaMemcpy idata to dev_afterScan failed!");
+
+	//for d = 1 to lg(n) do
+	for (int depth = 1; depth <= ilog2(n); ++depth)
+	{
+		parallelAdd << <numBlocks, blocksize >> >(n, depth, dev_afterScan, dev_beforeScan);
+		
+		//ping-pong buffers
+		int * temp = dev_afterScan;
+		dev_afterScan = dev_beforeScan;
+		dev_beforeScan = temp;
+	}
+
+	//because of ping-ponging, last iteration will be stored in beforeScan
+	cudaMemcpy(odata, dev_beforeScan, sizeof(int) * n, cudaMemcpyDeviceToHost);
+	checkCUDAError("cudaMemcpy dev_beforeScan to odata failed!");
+
+	cudaFree(dev_beforeScan);
+	cudaFree(dev_afterScan);
 }
 
 }
