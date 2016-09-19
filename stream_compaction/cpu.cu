d1d18b894150f7e1e3fa91dc44ac3792055a4ee6
Your Name <you@example.com>
2016-09-19 16:29:49 -0400

    Initial

--- stream_compaction/cpu.cu
+++ stream_compaction/cpu.cu
@@ -8,8 +8,10 @@ namespace CPU {
  * CPU scan (prefix sum).
  */
 void scan(int n, int *odata, const int *idata) {
-    // TODO
-    printf("TODO\n");
+	odata[0] = 0;
+	for (int i = 1; i < n; i++) {
+		odata[i] = odata[i - 1] + idata[i - 1];
+	}
 }
 
 /**
@@ -18,8 +20,13 @@ void scan(int n, int *odata, const int *idata) {
  * @returns the number of elements remaining after compaction.
  */
 int compactWithoutScan(int n, int *odata, const int *idata) {
-    // TODO
-    return -1;
+	int index = 0;
+	for (int i = 0; i < n; i++) {
+		if (idata[i] != 0) {
+			odata[index++] = idata[i];
+		}
+	}
+    return index;
 }
 
 /**
