d6974cdb7093e46a9197a2b3087b56ed15696b4f
GABRIEL NAGHI <gnaghi@dc1.base.seas.upenn.edu>
2016-09-20 17:00:21 -0400

    efficient compaction scatter and maptoboolen implemenations. Working.

--- stream_compaction/efficient.cu
+++ stream_compaction/efficient.cu
@@ -68,6 +68,7 @@ __global__ void downSweep(int n, int depth, int *data)
  * Performs prefix-sum (aka scan) on idata, storing the result into odata.
  */
 void scan(int n, int *odata, const int *idata) {
+	/* Safer way to do this would be to use cudaPointerGetAttributes to check if already on device*/
 
 	dim3 numBlocks = (n + blocksize - 1) / blocksize;
 	int rounded_n = powf(2, ilog2ceil(n));
@@ -107,7 +108,10 @@ void scan(int n, int *odata, const int *idata) {
 	cudaMemcpy(odata, dev_data, sizeof(int) * n, cudaMemcpyDeviceToHost);
 	checkCUDAError("cudaMemcpy dev_data to odata failed!");
 
+	cudaFree(dev_data);
 }
+
+
 
 /**
  * Performs stream compaction on idata, storing the result into odata.
@@ -119,8 +123,67 @@ void scan(int n, int *odata, const int *idata) {
  * @returns      The number of elements remaining after compaction.
  */
 int compact(int n, int *odata, const int *idata) {
-    // TODO
-    return -1;
+	dim3 numBlocks = (n + blocksize - 1) / blocksize;
+
+	//device pointers
+	int * dev_idata;
+	int * dev_odata;
+	int * dev_bools;
+	int * dev_indices;
+	cudaMalloc((void **)&dev_idata, n * sizeof(int));
+	checkCUDAError("cudaMalloc dev_idata failed!");
+	cudaMalloc((void **)&dev_odata, n * sizeof(int));
+	checkCUDAError("cudaMalloc dev_odata failed!");
+	cudaMalloc((void **)&dev_bools, n * sizeof(int));
+	checkCUDAError("cudaMalloc dev_bools failed!");
+	cudaMalloc((void **)&dev_indices, n * sizeof(int));
+	checkCUDAError("cudaMalloc dev_indices failed!");
+
+	//host pointers
+	int * bools = (int *)malloc(n * sizeof(int));
+	int * scanResult = (int *)malloc(n * sizeof(int));
+
+	//compute temp array of bools
+	cudaMemcpy(dev_idata, idata, sizeof(int) * n, cudaMemcpyHostToDevice);
+	checkCUDAError("cudaMemcpy idata to dev_idata failed!");
+	Common::kernMapToBoolean << < numBlocks, blocksize >> >(n, dev_bools, dev_idata);
+	checkCUDAError("kernMapToBoolean failed!");
+	cudaMemcpy(bools, dev_bools, sizeof(int) * n, cudaMemcpyDeviceToHost);
+	checkCUDAError("cudaMemcpy dev_bools to bools failed!");
+
+	
+	//run exclusive scan on temp array
+	scan(n, scanResult, bools);
+	cudaMemcpy(dev_indices, scanResult, sizeof(int) * n, cudaMemcpyHostToDevice);
+	checkCUDAError("cudaMemcpy scanResult to dev_indices failed!");
+
+	//scatter
+	cudaMemset(dev_odata, 0, n * sizeof(int));
+	Common::kernScatter << < numBlocks, blocksize >> >(n, dev_odata,
+		dev_idata, dev_bools, dev_indices);
+
+	cudaMemcpy(odata, dev_odata, n * sizeof(int), cudaMemcpyDeviceToHost);
+	checkCUDAError("cudaMemcpy dev_odata to odata failed!");
+
+	cudaFree(dev_bools);
+	cudaFree(dev_idata);
+	cudaFree(dev_odata);
+	cudaFree(dev_indices);
+	free(bools);
+	free(scanResult);
+
+	//count valid blocks. Only nonzero is valid
+	// This block of code feels really wrong and against the entire spirit of the project
+	for (int i = 0; i < n; ++i)
+	{
+		if (!odata[i])
+			return i;
+	}
+
+	return n;
+
+
+
 }
 
 }
