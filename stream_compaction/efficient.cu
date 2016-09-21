af6eccc9b3fbcbc62270c7bab3d30dc0424e8d82
Trung Le <trungtuanle90@gmail.com>
2016-09-20 20:15:14 -0400

    Implemented work-efficient scan

--- stream_compaction/efficient.cu
+++ stream_compaction/efficient.cu
@@ -3,17 +3,69 @@
 #include "common.h"
 #include "efficient.h"
 
+#define BLOCK_SIZE 128
+#define BLOCK_COUNT(n) ((n + BLOCK_SIZE - 1) / BLOCK_SIZE)
+
 namespace StreamCompaction {
 namespace Efficient {
 
 // TODO: __global__
+
+__global__ void upsweep(int n, int level, int* odata) {
+  int tid = threadIdx.x + (blockIdx.x * blockDim.x);
+  if (tid >= n) {
+    return;
+  }
+
+  int twoToLevel = powf(2, level);
+  int twoToLevelPlusOne = powf(2, level + 1);
+  if (tid % twoToLevelPlusOne == 0) {
+    odata[tid + twoToLevelPlusOne - 1] += odata[tid + twoToLevel - 1];
+  }
+}
+
+__global__ void downsweep(int n, int level, int* odata) {
+  int tid = threadIdx.x + (blockIdx.x * blockDim.x);
+  if (tid >= n) {
+    return;
+  }
+  int twoToLevel = powf(2, level);
+  int twoToLevelPlusOne = powf(2, level + 1);
+
+  if (tid % twoToLevelPlusOne == 0) {
+    int t = odata[tid + twoToLevel - 1];
+    odata[tid + twoToLevel - 1] = odata[tid + twoToLevelPlusOne - 1];
+    odata[tid + twoToLevelPlusOne - 1] += t;
+  }
+}
 
 /**
  * Performs prefix-sum (aka scan) on idata, storing the result into odata.
  */
 void scan(int n, int *odata, const int *idata) {
     // TODO
-    printf("TODO\n");
+  int* dev_odata;
+
+  int height = ilog2ceil(n);
+
+  int ceilPower2 = pow(2, height);
+  cudaMalloc((void**)&dev_odata, ceilPower2 * sizeof(int));
+  cudaMemset(dev_odata, 0, ceilPower2 * sizeof(int));
+  cudaMemcpy(dev_odata, idata, n * sizeof(int), cudaMemcpyHostToDevice);
+
+  for (int level = 0; level < height; ++level) {
+    upsweep << <BLOCK_COUNT(ceilPower2), BLOCK_SIZE >> >(ceilPower2, level, dev_odata);
+  }
+
+  cudaMemset(dev_odata + (ceilPower2 - 1), 0, sizeof(int));
+
+  for (int level = height - 1; level >= 0; --level) {
+    downsweep << <BLOCK_COUNT(ceilPower2), BLOCK_SIZE >> >(ceilPower2, level, dev_odata);
+  }
+
+  cudaMemcpy(odata, dev_odata, n * sizeof(int), cudaMemcpyDeviceToHost);
+
+  cudaFree(dev_odata);
 }
 
 /**
