a29fb3350d6cf287518a3ce56397300f83ba8c87
nischalkn <nischal@seas.upenn.edu>
2016-09-22 03:52:42 -0400

    trust scan added

--- stream_compaction/thrust.cu
+++ stream_compaction/thrust.cu
@@ -9,14 +9,19 @@
 namespace StreamCompaction {
 namespace Thrust {
 
-/**
- * Performs prefix-sum (aka scan) on idata, storing the result into odata.
- */
-void scan(int n, int *odata, const int *idata) {
-    // TODO use `thrust::exclusive_scan`
-    // example: for device_vectors dv_in and dv_out:
-    // thrust::exclusive_scan(dv_in.begin(), dv_in.end(), dv_out.begin());
-}
+	/**
+	 * Performs prefix-sum (aka scan) on idata, storing the result into odata.
+	 */
+	void scan(int n, int *odata, const int *idata) {
+		// TODO use `thrust::exclusive_scan`
+		// example: for device_vectors dv_in and dv_out:
+		// thrust::exclusive_scan(dv_in.begin(), dv_in.end(), dv_out.begin());
+		thrust::device_vector<int> dv_in(n);
+		thrust::device_vector<int> dv_out(n);
+		thrust::copy(idata, idata + n - 1, dv_in.begin());
+		thrust::exclusive_scan(dv_in.begin(), dv_in.end(), dv_out.begin());
+		thrust::copy(dv_out.begin(), dv_out.end(), odata);
+	}
 
 }
 }
