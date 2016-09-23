91be22ff9658ba7c37584fc9ecb6a0f6a9e5cf6e
Your Name <you@example.com>
2016-09-23 19:45:58 -0400

    Still working on efficient

--- stream_compaction/efficient.cu
+++ stream_compaction/efficient.cu
@@ -9,20 +9,27 @@ namespace Efficient {
 	__global__ void kernScanUp(int n, int d, int step, int *g_odata, int*g_idata) {
 		int index = (blockIdx.x * blockDim.x) + threadIdx.x;
 
-		if (index < d) {
-			int ai = step * (2 * index + 1) - 1;
-			int bi = step * (2 * index + 2) - 1;
+		// Calculate children indices
+		int ai = step * (2 * index) - 1;
+		int bi = step * (2 * index + 1) - 1;
 
-			g_idata[bi] += g_idata[ai];
+		if (ai >= n) {
+			printf("ai is too big");
 		}
+
+		if (bi >= n) {
+			printf("bi is too big");
+		}
+		g_idata[bi] += g_idata[ai];
+
+	}
+
+	__global__ void kernZeroLastValue(int n, int *data) {
+		data[n - 1] = 0;
 	}
 
 	__global__ void kernScanDown(int n, int d, int step, int *g_odata, int *g_idata) {
 		int index = (blockIdx.x * blockDim.x) + threadIdx.x;
-
-		if (index == 0) {
-			g_idata[n - 1] = 0;
-		}
 
 		// traverse down tree & build scan  
 		if (index < d) {
@@ -36,6 +43,7 @@ namespace Efficient {
 		}
 	}
 
+
 /**
  * Performs prefix-sum (aka scan) on idata, storing the result into odata.
  */
@@ -43,6 +51,7 @@ void scan(int n, int *odata, const int *idata) {
 	dim3 fullBlocksPerGrid((n + 128 - 1) / 128);
 	dim3 threadsPerBlock(128);
 
+	// Allocate GPU space
 	int* dev_in;
 	int* dev_out;
 
@@ -52,16 +61,25 @@ void scan(int n, int *odata, const int *idata) {
 	cudaMalloc((void**)&dev_out, n * sizeof(int));
 	checkCUDAError("cudaMalloc Error dev_out.");
 
+	// Copy array to device
 	cudaMemcpy(dev_in, idata, sizeof(int) * n, cudaMemcpyHostToDevice);
 	
+	// Scan up
 	for (int d = 0; d < ilog2ceil(n); d++) {
-		kernScanUp << <fullBlocksPerGrid, threadsPerBlock >> >(n, d, pow(2, d+1), dev_out, dev_in);
+		int n2 = pow(2, ilog2ceil(n) - d - 1);
+		fullBlocksPerGrid = dim3((n2 + 128 - 1) / 128);
+		kernScanUp << <fullBlocksPerGrid, threadsPerBlock >> >(n, d, pow(2, d), dev_out, dev_in);
 	}
 
+	// Make last value 0
+	kernZeroLastValue<<<1, 1>>>(n, dev_out);
+
+	// Scan down
 	for (int d = ilog2ceil(n); d >= 0; d--) {
 		kernScanDown << <fullBlocksPerGrid, threadsPerBlock >> >(n, d, pow(2, d + 1), dev_out, dev_in);
 	}
-		
+	
+	// Copy data back to host
 	cudaMemcpy(odata, dev_out, sizeof(int) * n, cudaMemcpyDeviceToHost);
 	checkCUDAError("memcpy back failed!");
 
