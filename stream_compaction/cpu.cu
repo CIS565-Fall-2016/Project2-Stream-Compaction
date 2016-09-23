e249e876dc42b006db015de3ddd7d29bfcdc7c34
immiao <miaokaixiang@126.com>
2016-09-23 15:40:23 -0400

    Finished cpu.cu

--- stream_compaction/cpu.cu
+++ stream_compaction/cpu.cu
@@ -1,4 +1,5 @@
 #include <cstdio>
+#include "stream_compaction\common.h"
 #include "cpu.h"
 
 namespace StreamCompaction {
@@ -9,7 +10,36 @@ namespace CPU {
  */
 void scan(int n, int *odata, const int *idata) {
     // TODO
-    printf("TODO\n");
+	if (n <= 0)
+		return;
+
+	memcpy(odata, idata, n * sizeof(int));
+	int nCeilLog = ilog2ceil(n);
+
+	int nLength = 1 << nCeilLog;
+
+	for (int d = 0; d < nCeilLog; d++)
+		for (int k = 0; k < nLength; k++)
+		{
+			int m = 1 << (d + 1);
+			if (!(k % m))
+				odata[k + m - 1] += odata[k + (m >> 1) - 1];
+		}
+
+	odata[nLength - 1] = 0;
+	for (int d = nCeilLog - 1; d >= 0; d--)
+		for (int k = 0; k < nLength; k++)
+		{
+			int m = 1 << (d + 1);
+			if (!(k % m))
+			{
+				int index1 = k + (m >> 1) - 1;
+				int index2 = k + m - 1;
+				int temp = odata[index1];
+				odata[index1] = odata[index2];
+				odata[index2] += temp;
+			}
+		}
 }
 
 /**
@@ -19,7 +49,15 @@ void scan(int n, int *odata, const int *idata) {
  */
 int compactWithoutScan(int n, int *odata, const int *idata) {
     // TODO
-    return -1;
+	if (n <= 0)
+		return -1;
+	int counter = 0;
+	for (int i = 0; i < n; i++)
+	{
+		if (idata[i])
+			odata[counter++] = idata[i];
+	}
+	return counter;
 }
 
 /**
@@ -29,7 +67,19 @@ int compactWithoutScan(int n, int *odata, const int *idata) {
  */
 int compactWithScan(int n, int *odata, const int *idata) {
     // TODO
-    return -1;
+	if (n <= 0)
+		return -1;
+	int counter = 0;
+	
+	for (int i = 0; i < n; i++)
+		odata[i] = idata[i] ? 1 : 0;
+	scan(n, odata, odata);
+	for (int i = 0; i < n - 1; i++)
+	{
+		if (odata[i] != odata[i + 1])
+			odata[counter++] = idata[i];
+	}
+	return counter;
 }
 
 }
