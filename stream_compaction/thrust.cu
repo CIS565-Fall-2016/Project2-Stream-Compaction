2b45f4bdcd37ed11b7879fdf6cd4c978a9bbf958
immiao <miaokaixiang@126.com>
2016-09-23 22:08:05 -0400

    Finished thrust.cu

--- stream_compaction/thrust.cu
+++ stream_compaction/thrust.cu
@@ -9,6 +9,8 @@
 namespace StreamCompaction {
 namespace Thrust {
 
+int *dev_Data;
+int *dev_OutputData;
 /**
  * Performs prefix-sum (aka scan) on idata, storing the result into odata.
  */
@@ -16,6 +18,7 @@ void scan(int n, int *odata, const int *idata) {
     // TODO use `thrust::exclusive_scan`
     // example: for device_vectors dv_in and dv_out:
     // thrust::exclusive_scan(dv_in.begin(), dv_in.end(), dv_out.begin());
+	thrust::exclusive_scan(idata, idata + n, odata);
 }
 
 }
