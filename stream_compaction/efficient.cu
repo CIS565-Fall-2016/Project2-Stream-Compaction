1342e801bddff6b83c63e52f18adb28b0c308294
immiao <miaokaixiang@126.com>
2016-09-23 20:20:21 -0400

    Finished efficient scan

--- stream_compaction/efficient.cu
+++ stream_compaction/efficient.cu
@@ -11,9 +11,59 @@ namespace Efficient {
 /**
  * Performs prefix-sum (aka scan) on idata, storing the result into odata.
  */
+
+int *dev_Data;
+
+__global__ void CudaUpSweep(int d, int *data)
+{
+	int thid = threadIdx.x;
+	int m = 1 << (d + 1);
+	if (!(thid % m))
+		data[thid + m - 1] += data[thid + (m >> 1) - 1];
+}
+
+__global__ void CudaDownSweep(int d, int *data)
+{
+	int thid = threadIdx.x;
+	int m = 1 << (d + 1);
+	if (!(thid % m))
+	{
+		int temp = data[thid + (m >> 1) - 1];
+		data[thid + (m >> 1) - 1] = data[thid + m - 1];
+		data[thid + m - 1] += temp;
+	}
+}
+
 void scan(int n, int *odata, const int *idata) {
-    // TODO
-    printf("TODO\n");
+ //   int n = 8;
+	//int idata[8] ={0,1,2,3,4,5,6,7};
+	//int odata[8];
+	int nCeilLog = ilog2ceil(n);
+	int nLength = 1 << nCeilLog;
+
+	cudaMalloc((void**)&dev_Data, nLength * sizeof(int));
+	checkCUDAError("cudaMalloc failed!");
+
+	cudaMemcpy(dev_Data, idata, sizeof(int) * nLength, cudaMemcpyHostToDevice);
+	checkCUDAError("cudaMemcpy to device failed!");
+
+	for (int i = 0; i < nCeilLog; i++)
+		CudaUpSweep<<<1, nLength>>>(i, dev_Data);
+
+	cudaMemset(dev_Data + nLength - 1, 0, sizeof(int));
+	checkCUDAError("cudaMemcpy to device failed!");
+	for (int i = nCeilLog - 1; i >= 0; i--)
+	{
+		CudaDownSweep<<<1, nLength>>>(i, dev_Data);
+				//cudaMemcpy(odata, dev_Data, sizeof(int) * (nLength), cudaMemcpyDeviceToHost);
+
+	}
+
+	cudaMemcpy(odata, dev_Data, sizeof(int) * nLength, cudaMemcpyDeviceToHost);
+		//	for (int j = 0; j < n; j++)
+		//	printf("%d ", odata[j]);
+		//printf("\n");
+	checkCUDAError("cudaMemcpy to host failed!");	
 }
 
 /**
--- stream_compaction/naive.cu
+++ stream_compaction/naive.cu
@@ -11,10 +11,45 @@ namespace Naive {
 /**
  * Performs prefix-sum (aka scan) on idata, storing the result into odata.
  */
-void scan(int n, int *odata, const int *idata) {
-    // TODO
-    printf("TODO\n");
+
+int *dev_Data[2];
+
+__global__ void CudaScan(int d, int *in, int *out)
+{
+	int thid = threadIdx.x;
+	int m = 1 << (d - 1);
+		
+	if (thid >= m)
+		out[thid] = in[thid] + in[thid - m];
+	else
+		out[thid] = in[thid];
+
 }
+
+void scan(int n, int *odata, const int *idata) {
+
+	int nCeilLog = ilog2ceil(n);
+	int nLength = 1 << nCeilLog;
+
+	cudaMalloc((void**)&dev_Data[0], nLength * sizeof(int));
+	cudaMalloc((void**)&dev_Data[1], nLength * sizeof(int));
+	checkCUDAError("cudaMalloc failed!");
+
+	cudaMemcpy(dev_Data[0], idata, sizeof(int) * nLength, cudaMemcpyHostToDevice);
+	checkCUDAError("cudaMemcpy to device failed!");
+
+	int nOutputIndex = 0;
+	for (int i = 1; i <= nCeilLog; i++)
+	{
+		nOutputIndex ^= 1;
+		CudaScan<<<1, nLength>>>(i, dev_Data[nOutputIndex ^ 1], dev_Data[nOutputIndex]);
+	}
+	odata[0] = 0;
+	cudaMemcpy(odata + 1, dev_Data[nOutputIndex], sizeof(int) * (nLength - 1), cudaMemcpyDeviceToHost);
+	checkCUDAError("cudaMemcpy to host failed!");	
+}
+
+
 
 }
 }
