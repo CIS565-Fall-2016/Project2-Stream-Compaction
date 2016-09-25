2a13979feeaa1294c6bf3fc3f07e6261206f16f9
Daniel Krupka <krupkad@seas.upenn.edu>
2016-09-25 12:44:34 -0400

    finished cpu implementations

--- stream_compaction/cpu.cu
+++ stream_compaction/cpu.cu
@@ -8,8 +8,11 @@ namespace CPU {
  * CPU scan (prefix sum).
  */
 void scan(int n, int *odata, const int *idata) {
-    // TODO
-    printf("TODO\n");
+    int t = 0;
+    for (int i = 0; i < n; i++) {
+      odata[i] = t;
+      t += idata[i];
+    }
 }
 
 /**
@@ -18,8 +21,14 @@ void scan(int n, int *odata, const int *idata) {
  * @returns the number of elements remaining after compaction.
  */
 int compactWithoutScan(int n, int *odata, const int *idata) {
-    // TODO
-    return -1;
+    int oIdx = 0;
+    for (int i = 0; i < n; i++) {
+      if (idata[i] != 0) {
+        odata[oIdx] = idata[i];
+        oIdx++;
+      }
+    }
+    return oIdx;
 }
 
 /**
@@ -28,8 +37,25 @@ int compactWithoutScan(int n, int *odata, const int *idata) {
  * @returns the number of elements remaining after compaction.
  */
 int compactWithScan(int n, int *odata, const int *idata) {
-    // TODO
-    return -1;
+    int *keep = new int[n];
+    for (int i = 0; i < n; i++) {
+      keep[i] = (idata[i] != 0) ? 1 : 0;
+    }
+
+    int *keepScan = new int[n];
+    int nKeep = 0;
+    scan(n, keepScan, keep);
+    for (int i = 0; i < n; i++) {
+      if (!keep[i])
+        continue;
+
+      nKeep++;
+      odata[keepScan[i]] = idata[i];
+    }
+
+    delete keepScan;
+    delete keep;
+    return nKeep;
 }
 
 }
