c8298eb619b4a23164724bfcf663f03dd34a732c
YOUR NAME <GITHUB_USER@users.noreply.github.com>
2016-09-25 16:01:17 -0400

    coding almost done

--- stream_compaction/efficient.cu
+++ stream_compaction/efficient.cu
@@ -5,15 +5,69 @@
 
 namespace StreamCompaction {
 namespace Efficient {
-
+#define blockSize 128
 // TODO: __global__
+__global__ void upSweep(int offset, int n,   int *idata){
+	int index = threadIdx.x + blockIdx.x*blockDim.x;
+	if (index >=n) return;
+	int tmp=(offset << 1);
+	if (index % tmp==0){
+		if (index + tmp <n){
+			if (index >= offset){
+				idata[index+(tmp << 1)-1] = idata[index+offset-1] + idata[index+tmp-1];
+			}
+		}
+	}
+}
 
+__global__ void downSweep(int offset, int n,  int *idata){
+	int index = threadIdx.x + blockIdx.x*blockDim.x;
+	if (index >=n) return;
+	int tmp=(offset << 1);
+	if (index % tmp==0){
+		if (index + tmp <n){
+			int t = idata[index + offset -1];
+			idata[index+offset-1] = idata[index+ tmp -1];
+			idata[index+ tmp -1] += t ;
+		}
+ 
+	}
+}
 /**
  * Performs prefix-sum (aka scan) on idata, storing the result into odata.
  */
 void scan(int n, int *odata, const int *idata) {
     // TODO
-    printf("TODO\n");
+    //printf("TODO\n");
+	int levels_max = ilog2ceil(n);
+	int n_max= 2 << levels_max;
+
+	dim3 numblocks(std::ceil((double) n_max / blockSize));
+	int* idata_buff;
+	//allocate more space than needed
+	cudaMalloc((void**)&idata_buff, n_max*sizeof(int)); 	
+		checkCUDAError("cudaMalloc-idata_buff-  failed!");	
+	//reset all to zeros
+    cudaMemset(idata_buff, 0, n_max*sizeof(int));
+		checkCUDAError("cudaMemset-idata_buff-  failed!");	
+
+	/// CPU -->GPU
+	cudaMemcpy(idata_buff,idata,n*sizeof(int),cudaMemcpyHostToDevice);
+		checkCUDAError("cudaMemcpy-idata_buff-failed");
+	//upsweep
+	for (int level=0; level <= levels_max-1; level++){
+		upSweep<<<numblocks,blockSize>>>(1<<level+1, n_max, idata_buff);
+	}
+	//downsweep
+	//set root x[n-1]=0
+	idata_buff[n_max-1]=0;
+	for (int level=levels_max-1; level >=0 ; level--){
+		downSweep<<<numblocks,blockSize>>>(1<<level+1, n_max, idata_buff);
+	}
+	/// GPU --> CPU
+	cudaMemcpy(odata, idata_buff, n*sizeof(int),cudaMemcpyDeviceToHost);
+		checkCUDAError("cudaMemcpy-odata-failed");
+	cudaFree(idata_buff);
 }
 
 /**
@@ -26,8 +80,46 @@ void scan(int n, int *odata, const int *idata) {
  * @returns      The number of elements remaining after compaction.
  */
 int compact(int n, int *odata, const int *idata) {
-    // TODO
-    return -1;
+    int n_remaing=0;
+	int * idata_buff;
+	int * odata_buff;
+	int * bool_buff;
+	int * indices_buff;
+
+	dim3 numblocks(std::ceil((double) n/blockSize));
+	//
+	cudaMalloc((void**)&idata_buff,n * sizeof(int));
+		checkCUDAError("cudaMalloc-idata_buff-failed");
+	cudaMalloc((void**)&odata_buff,n * sizeof(int));
+		checkCUDAError("cudaMalloc-odata_buff-failed");
+	cudaMalloc((void**)&bool_buff,n * sizeof(int));
+		checkCUDAError("cudaMalloc-odata_buff-failed");
+	cudaMalloc((void**)&indices_buff,n * sizeof(int));
+		checkCUDAError("cudaMalloc-odata_buff-failed");
+
+	cudaMemcpy(idata_buff, idata, n* sizeof(int), cudaMemcpyHostToDevice);
+		checkCUDAError("cudaMemcpy-idata_buff-failed");
+	cudaMemcpy(odata_buff, odata, n* sizeof(int), cudaMemcpyHostToDevice);
+		checkCUDAError("cudaMemcpy-odata_buff-failed");
+
+	//produce the indices
+	Common::kernMapToBoolean<<<numblocks, blockSize>>> ( n, bool_buff, idata_buff);
+	scan<<<numblocks, blockSize>>>(n, indices_buff, bool_buff);
+	Common::kernScatter<<<numblocks, blockSize>>>( n, odata_buff, idata_buff,  bool_buff,  indices_buff);
+
+
+	//GPU-->CPU
+	cudaMemcpy(odata,odata_buff,n*sizeof(int),cudaMemcpyDeviceToHost);
+
+	for (int i =0; i< n; i++){
+		n_remaing+=bool_buff[i];
+	}
+
+	cudaFree(idata_buff);
+	cudaFree(odata_buff);
+	cudaFree(bool_buff);
+	cudaFree(indices_buff);
+    return n_remaing;
 }
 
 }
--- stream_compaction/naive.cu
+++ stream_compaction/naive.cu
@@ -1,19 +1,76 @@
 #include <cuda.h>
 #include <cuda_runtime.h>
+#include <algorithm>
+
 #include "common.h"
 #include "naive.h"
 
 namespace StreamCompaction {
 namespace Naive {
 
-// TODO: __global__
+#define blockSize 128
+//__global__
+__global__ void scan(int offset, int n, int *odata, const int *idata) {
+	int index = threadIdx.x + blockIdx.x*blockDim.x;
+	if (index >=n) return;
 
+	if (index >= offset){
+		odata[index] = idata[index] + idata[index-offset];
+	}
+	else{
+		odata[index] = idata[index];
+	}
+}
+__global__ void excludesiveShift(int n, int *odata, int *idata){
+	int index = threadIdx.x + blockIdx.x* blockDim.x;
+	if (index>=n) return;
+	if (index>=1){
+		odata[index]= idata[index-1] ;
+	}
+	else {
+		odata[index]= 0;
+	}
+}
 /**
  * Performs prefix-sum (aka scan) on idata, storing the result into odata.
  */
 void scan(int n, int *odata, const int *idata) {
-    // TODO
-    printf("TODO\n");
+ 
+	//dim3 numblocks(std::ceil((double) n / blockSize));
+	dim3 numblocks((n + blockSize - 1) / blockSize);
+	int* idata_buff;
+	int* odata_buff;
+
+	cudaMalloc((void**)&idata_buff, n*sizeof(int));
+	checkCUDAError("cudaMalloc-idata_buff-  failed!");	
+	cudaMalloc((void**)&odata_buff, n*sizeof(int));
+	checkCUDAError("cudaMalloc-odata_buff-failed!");
+
+	/// CPU -->GPU
+	cudaMemcpy(idata_buff,idata,sizeof(int)*n,cudaMemcpyHostToDevice);
+	cudaMemcpy(odata_buff,idata,sizeof(int)*n,cudaMemcpyHostToDevice);
+
+	 
+	for (int level= 1; level <= ilog2ceil(n); level++) {
+		int offset; 
+		if (level==1){
+			offset = 1;
+		}
+		else {
+			offset = 2 << (level -2);
+		}
+		// for the given level, all threads read from idata_buff
+		scan <<<numblocks, blockSize>>>(offset, n, odata_buff, idata_buff);
+		//std::swap(idata_buff, odata_buff);
+		// odata_buff --> idata_buff for next iteration
+		cudaMemcpy(idata_buff, odata_buff, sizeof(int)*n, cudaMemcpyDeviceToDevice);
+	}
+	excludesiveShift<<<numblocks, blockSize>>>(n, odata_buff, idata_buff);
+
+	//GPU --> CPU 	
+	cudaMemcpy(odata, odata_buff, sizeof(int)*n, cudaMemcpyDeviceToHost);
+	cudaFree(idata_buff);
+	cudaFree(odata_buff);
 }
 
 }
