4960f0a488f4176f54080ce9427c8f8e8d409b63
Daniel Krupka <krupkad@seas.upenn.edu>
2016-09-25 14:21:47 -0400

    finished efficient scan

--- stream_compaction/efficient.cu
+++ stream_compaction/efficient.cu
@@ -6,14 +6,65 @@
 namespace StreamCompaction {
 namespace Efficient {
 
-// TODO: __global__
+// perform reduction
+__global__ void kernScanUp(int n, int dPow, int *data) {
+  int k = blockIdx.x * blockDim.x + threadIdx.x;
+  if (k % dPow != 0 || k + dPow - 1 >= n)
+    return;
+
+  data[k + dPow - 1] += data[k + dPow/2 - 1];
+}
+
+
+// perform reduction
+__global__ void kernScanDown(int n, int dPow, int *data) {
+  int k = blockIdx.x * blockDim.x + threadIdx.x;
+  if (k % dPow != 0 || k + dPow - 1 >= n)
+    return;
+
+  int t = data[k + dPow/2 - 1];
+  data[k + dPow/2 - 1] = data[k + dPow - 1];
+  data[k + dPow - 1] += t;
+}
 
 /**
  * Performs prefix-sum (aka scan) on idata, storing the result into odata.
  */
 void scan(int n, int *odata, const int *idata) {
-    // TODO
-    printf("TODO\n");
+  dim3 blkDim(256);
+  dim3 blkCnt((n + blkDim.x - 1)/blkDim.x);
+
+  unsigned int pot = n;
+  pot--;
+  pot |= pot >> 1;
+  pot |= pot >> 2;
+  pot |= pot >> 4;
+  pot |= pot >> 8;
+  pot |= pot >> 16;
+  pot++;
+
+  int *devData;
+  cudaMalloc((void**)&devData, pot*sizeof(int));
+  cudaMemset(devData, 0, pot*sizeof(int));
+  cudaMemcpy(devData, idata, n*sizeof(int), cudaMemcpyHostToDevice);
+
+  int dPow = 2;
+  while (dPow/2 < n) {
+    kernScanUp<<<blkCnt,blkDim>>>(pot, dPow, devData);
+    dPow *= 2;
+  }
+  cudaMemset(&devData[pot-1], 0, sizeof(int));
+
+  while (dPow > 1) {
+    kernScanDown<<<blkCnt,blkDim>>>(pot, dPow, devData);
+    dPow /= 2;
+  }
+  cudaMemcpy(odata, devData, n*sizeof(int), cudaMemcpyDeviceToHost);
+  for (int i = 0; i < 10; i++)
+    printf("%d ", odata[i]);
+  printf("\n");
+
+  cudaFree(devData);
 }
 
 /**
