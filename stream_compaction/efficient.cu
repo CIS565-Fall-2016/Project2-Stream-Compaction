b653d6dc5a7522de4ff7314f193a243cd3ed09d4
Your Name <you@example.com>
2016-09-25 17:19:40 -0400

    Need way to keep track of final compacted size

--- stream_compaction/efficient.cu
+++ stream_compaction/efficient.cu
@@ -5,7 +5,6 @@
 
 namespace StreamCompaction {
 namespace Efficient {
-
 	__global__ void kernScanUp(int n, int pow_2_d, int pow_2_d_one, int*g_idata) {
 		int index = (blockIdx.x * blockDim.x) + threadIdx.x;
 
@@ -33,6 +32,30 @@ namespace Efficient {
 		float t = g_idata[ai];
 		g_idata[ai] = g_idata[bi];
 		g_idata[bi] += t;
+	}
+
+	__global__ void kernMap(int n, int *idata, int *odata) {
+		int index = (blockIdx.x * blockDim.x) + threadIdx.x;
+
+		if (index < n) {
+			if (idata[index] != 0) {
+				odata[index] = 1;
+			}
+			else {
+				odata[index] = 0;
+			}
+		}
+	}
+
+	__global__ void kernScatter(int n, int *idata, int *odata, int *imap, int *iscan) {
+		int index = (blockIdx.x * blockDim.x) + threadIdx.x;
+
+		if (index < n) {
+			if (imap[index] == 1) {
+				odata[iscan[index]] = idata[index];
+			}
+		}
+
 	}
 
 
@@ -89,7 +112,48 @@ void scan(int n, int *odata, const int *idata) {
  * @returns      The number of elements remaining after compaction.
  */
 int compact(int n, int *odata, const int *idata) {
-    // TODO
+	dim3 fullBlocksPerGrid((n + 128 - 1) / 128);
+	dim3 threadsPerBlock(128);
+
+	// Allocate GPU space
+	int* dev_in;
+	int* dev_out;
+	int* dev_map;
+	int* dev_scan;
+
+	cudaMalloc((void**)&dev_in, n * sizeof(int));
+	checkCUDAError("cudaMalloc Error dev_in.");
+
+	cudaMalloc((void**)&dev_out, n * sizeof(int));
+	checkCUDAError("cudaMalloc Error dev_in.");
+
+	cudaMalloc((void**)&dev_map, n * sizeof(int));
+	checkCUDAError("cudaMalloc Error dev_map.");
+
+	cudaMalloc((void**)&dev_scan, n * sizeof(int));
+	checkCUDAError("cudaMalloc Error dev_map.");
+
+	// Copy array to device
+	cudaMemcpy(dev_in, idata, sizeof(int) * n, cudaMemcpyHostToDevice);
+
+	// Map 1s and 0s
+	kernMap << < fullBlocksPerGrid, threadsPerBlock>>>(n, dev_in, dev_map);
+
+	// Scan the map
+	scan(n, dev_scan, dev_map);
+
+	// Scatter
+	kernScatter << <fullBlocksPerGrid, threadsPerBlock >> >(n, dev_in, dev_out, dev_map, dev_scan);
+
+	// Copy data back to host
+	cudaMemcpy(odata, dev_map, sizeof(int) * n, cudaMemcpyDeviceToHost);
+	checkCUDAError("memcpy back failed!");
+
+	cudaFree(dev_in);
+	cudaFree(dev_out);
+	cudaFree(dev_scan);
+	cudaFree(dev_map);
+
     return -1;
 }
 
