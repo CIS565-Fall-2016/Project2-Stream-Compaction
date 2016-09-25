ed2c4640ce7dcb9d88e79486db92af25b583d0a0
Daniel Krupka <krupkad@seas.upenn.edu>
2016-09-25 15:07:00 -0400

    finished thrust scan

--- stream_compaction/thrust.cu
+++ stream_compaction/thrust.cu
@@ -16,6 +16,11 @@ void scan(int n, int *odata, const int *idata) {
     // TODO use `thrust::exclusive_scan`
     // example: for device_vectors dv_in and dv_out:
     // thrust::exclusive_scan(dv_in.begin(), dv_in.end(), dv_out.begin());
+
+    thrust::device_vector<int> devIn(idata, idata+n), devOut(n);
+    thrust::exclusive_scan(devIn.begin(), devIn.end(), devOut.begin());
+
+    thrust::copy(devOut.begin(), devOut.end(), odata);
 }
 
 }
