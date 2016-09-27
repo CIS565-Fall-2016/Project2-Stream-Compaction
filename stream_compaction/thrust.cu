9d0676779dae7e1705e768cb712cef2d7985b427
nischalkn <nischal@seas.upenn.edu>
2016-09-27 08:59:02 -0400

    thrust implementation for sorting

--- stream_compaction/thrust.cu
+++ stream_compaction/thrust.cu
@@ -3,6 +3,7 @@
 #include <thrust/device_vector.h>
 #include <thrust/host_vector.h>
 #include <thrust/scan.h>
+#include <thrust/sort.h>
 #include "common.h"
 #include "thrust.h"
 
@@ -19,7 +20,48 @@ namespace Thrust {
 		thrust::device_vector<int> dv_in(n);
 		thrust::device_vector<int> dv_out(n);
 		thrust::copy(idata, idata + n - 1, dv_in.begin());
+
+		#if PROFILE
+		cudaEvent_t start, stop;
+		cudaEventCreate(&start);
+		cudaEventCreate(&stop);
+		cudaEventRecord(start);
+		#endif
+
 		thrust::exclusive_scan(dv_in.begin(), dv_in.end(), dv_out.begin());
+
+		#if PROFILE
+		cudaEventRecord(stop);
+		cudaEventSynchronize(stop);
+		float milliseconds = 0;
+		cudaEventElapsedTime(&milliseconds, start, stop);
+		std::cout << "Time Elapsed for trust scan (size " << n << "): " << milliseconds << std::endl;
+		#endif
+
+		thrust::copy(dv_out.begin(), dv_out.end(), odata);
+	}
+
+	void sort(int n, int *odata, const int *idata) {
+		thrust::device_vector<int> dv_out(n);
+		thrust::copy(idata, idata + n, dv_out.begin());
+
+		#if PROFILE
+		cudaEvent_t start, stop;
+		cudaEventCreate(&start);
+		cudaEventCreate(&stop);
+		cudaEventRecord(start);
+		#endif
+
+		thrust::sort(dv_out.begin(), dv_out.end());
+
+		#if PROFILE
+		cudaEventRecord(stop);
+		cudaEventSynchronize(stop);
+		float milliseconds = 0;
+		cudaEventElapsedTime(&milliseconds, start, stop);
+		std::cout << "Time Elapsed for trust sort (size " << n << "): " << milliseconds << std::endl;
+		#endif
+
 		thrust::copy(dv_out.begin(), dv_out.end(), odata);
 	}
 
--- stream_compaction/thrust.h
+++ stream_compaction/thrust.h
@@ -3,5 +3,6 @@
 namespace StreamCompaction {
 namespace Thrust {
     void scan(int n, int *odata, const int *idata);
+	void sort(int n, int *odata, const int *idata);
 }
 }
